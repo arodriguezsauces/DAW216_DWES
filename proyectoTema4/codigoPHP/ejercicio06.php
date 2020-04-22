34218d47ae41ecbfe7ee4a13ffe6cfd34330ac08
Rodrigo <Rodrigo@LAPTOP-TLL57683>
2020-04-22 09:24:35 +0200

    Creacion ejercicio06

---
+++ proyectoTema4/codigoPHP/ejercicio06.php
@@ -0,0 +1,42 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="icon" type="image/png" href="../webroot/images/favicon.png">
+        <link href="../webroot/css/estiloDWES3.css"  rel="stylesheet" type="text/css" title="Default style">
+        <title>ejercicio 06 - Rodrigo Robles</title>
+    </head>
+    <body>
+        <h1>Rodrigo Robles</h1>
+        <?php
+        /* 
+        @author: Rodrigo Robles Miñambres
+        @since: 22/04/2020.
+        @description: Pagina web que cargue registros en la tabla Departamento desde un array departamentosnuevos
+            utilizando una consulta preparada.
+        */
+            
+        //incluir el archivo que contiene los datos de la conexion con la base de datos 
+        include_once '../config/datosBase.php';
+        
+        echo "<h2>Inserción de array de departamentos mediante una consulta preparada</h2>";
+        
+        //probamos la conexion
+        try {
+            
+            //la variable 'base' es una nuevo PDO que contiene los datos necesarios para establecer la conexion (DNS= host[ip] y base de datos), USUARIO[de la base de datos], CONTRASEÑA[del usuario])
+            $base = new PDO(DNS,USER,PWD);
+            $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //1.Reporta los errores, 2.Lanza las excepciones
+
+            $base=null; //Se cierra la conexion
+            
+        //si la conexion es erronea, capturamos el motivo mediante el catch y lo sacamos por pantalla    
+        } catch (PDOException $error) {
+            echo 'Error: ' . $error->getMessage() . '</br>';
+            die();//con esta funcion obligamos a terminar el codigo
+        }
+        
+
+        ?>
+    </body>
+    
+</html>
--- proyectoTema4/mostrarcodigo/mostrarEjercicio06.php
+++ proyectoTema4/mostrarcodigo/mostrarEjercicio06.php
@@ -3,11 +3,11 @@
         <meta charset="UTF-8">
         <link rel="icon" type="image/png" href="../webroot/images/favicon.png">
         
-        <title>MostrarEjercicio 05 - Rodrigo Robles</title>
+        <title>MostrarEjercicio 06 - Rodrigo Robles</title>
     </head>
     <body>
         <?php
-            $filename="../codigoPHP/ejercicio05.php"; //variable que contendra el archivo del que queremos ver el codigo
+            $filename="../codigoPHP/ejercicio06.php"; //variable que contendra el archivo del que queremos ver el codigo
             highlight_file($filename); //mostrar el contenido de la variable
         ?>
     </body>
