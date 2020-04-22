d86055a785cbcdf70fb7f19fac6acc2d91bd59fa
Rodrigo <Rodrigo@LAPTOP-TLL57683>
2020-04-22 10:49:12 +0200

    Finalizacion del ejercicio06

--- proyectoTema4/codigoPHP/ejercicio06.php
+++ proyectoTema4/codigoPHP/ejercicio06.php
@@ -27,6 +27,40 @@
             $base = new PDO(DNS,USER,PWD);
             $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //1.Reporta los errores, 2.Lanza las excepciones
 
+            $departamentosnuevos = [array("SSA", "Dep 61"), array("SSB","Dep 62"), array("SSC", "Dep 63")];
+            
+            //Insertar el nuevo departamento en la base de datos
+            $insertarSQL = 'INSERT INTO Departamento(CodDepartamento, DescDepartamento) VALUES (?,?)'; //en el query tengo marcadores de posicion (?, en los cuales se introduciran los campos al ejecutar el query
+
+
+            
+            foreach ($departamentosnuevos as $valores=>$valor) {
+
+                //encadeno el prepare y el execute, en este último indico los campos que introducire en el query, en las posicione que ocupan los ?
+                   $base->prepare($insertarSQL)->execute([$valor[0],$valor[1]]);                       
+            }
+            
+            
+             $mostrarSQL = 'SELECT CodDepartamento, DescDepartamento FROM Departamento ORDER BY CodDepartamento';
+                
+                 echo "<h2>Contenido de la tabla Departamentos</h2>";
+                       echo "<table>";
+
+                            echo "<tr>";
+
+                                echo "<th>Código</th>";
+                                echo "<th>Descripción</th>";
+
+                            echo "</tr>";
+
+                                foreach ($base->query($mostrarSQL) as $row) {
+                                    echo "<tr>";
+                                        print "<td>" . $row['CodDepartamento'] . "</td>" ; 
+                                        print "<td>" . $row['DescDepartamento'] . "</td>" ;
+                                    echo "</tr>";
+                                }
+
+                       echo "</table>";
             $base=null; //Se cierra la conexion
             
         //si la conexion es erronea, capturamos el motivo mediante el catch y lo sacamos por pantalla    
